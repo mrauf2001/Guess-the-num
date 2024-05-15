3fd2ba2b21dff827e5f963061620695eeadb44d2
Muhammad Rauf <168097042+mrauf2001@users.noreply.github.com>
2024-05-15 14:30:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var myNum;
+document.write("the  num before initialization :" + myNum);
+myNum=7;
+document.write('The Num After Initialize '+myNum);
+ myNum ++;
+ document.write("<br>" +myNum +"<br>")
+var newNum=myNum+5;
+ document.write(newNum);
+ newNum--;
+ document.write("<br>"+newNum)
+ var reminder = newNum/4;
+ document.write("<br>"+ reminder);
+ document.write("<br>"+ reminder*10)
